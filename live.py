408be188caa95d15d8f08d680cad9c98fdfa68bf
Stano Turza <stanislav.turza@datament.sk>
2019-05-02 10:02:31 +0200

    reformat, fix warnings, typos

--- live.py
+++ live.py
@@ -20,10 +20,12 @@ except:
     logger.log.info('exception, 7 is ok')
     _a_live_epg_next = 7
 
-EPG_GAP = 5 #gap for previous program
+EPG_GAP = 5  # gap for previous program
+
 
 def get_url(**kwargs):
-	return '{0}?{1}'.format(_url, urllib.urlencode(kwargs, 'utf-8'))
+    return '{0}?{1}'.format(_url, urllib.urlencode(kwargs, 'utf-8'))
+
 
 def channels(sl):
     channels = utils.call(sl, lambda: sl.channels(False))
@@ -31,13 +33,14 @@ def channels(sl):
     if channels:
         for channel in channels:
             list_item = xbmcgui.ListItem(label=channel['title'])
-            list_item.setInfo('video', {'title': channel['title']}) #TODO - genre?
+            list_item.setInfo('video', {'title': channel['title']})  # TODO - genre?
             list_item.setArt({'thumb': utils.get_logo(channel['title'], sl)})
-            list_item.setProperty('IsPlayable','true')
+            list_item.setProperty('IsPlayable', 'true')
             link = get_url(live='play', lid=channel['id'], stationid=channel['stationid'], askpin=channel['pin'])
             is_folder = False
             xbmcplugin.addDirectoryItem(_handle, link, list_item, is_folder)
     xbmcplugin.endOfDirectory(_handle)
+
 
 def play(sl, lid, stationid, askpin):
     if askpin != 'False':
@@ -46,15 +49,15 @@ def play(sl, lid, stationid, askpin):
             xbmcplugin.setResolvedUrl(_handle, False, xbmcgui.ListItem())
             return
 
-    #improvised epg
+    # improvised epg
     def get_plot_line(start, title):
         return '[B]' + start.strftime('%H:%M').decode('UTF-8') + '[/B] ' + title + '[CR]'
 
+    plot = ''
     today = datetime.datetime.now()
-    epg = utils.call(sl, lambda: sl.epg([{'stationid':stationid}], today, today + datetime.timedelta(days=2)))
+    epg = utils.call(sl, lambda: sl.epg([{'stationid': stationid}], today, today + datetime.timedelta(days=2)))
     if epg:
         now = datetime.datetime.now()
-        plot = ''
         last_program = None
         items_left = _a_live_epg_next
         for program in epg[0][stationid]:
@@ -63,19 +66,17 @@ def play(sl, lid, stationid, askpin):
             if show_item:
                 if last_program is not None:
                     last_start = datetime.datetime.fromtimestamp(last_program['start'])
-                    if last_start + datetime.timedelta(minutes=last_program['duration']+EPG_GAP) > now:
-                        plot += + get_plot_line(last_start,last_program['title'])
+                    if last_start + datetime.timedelta(minutes=last_program['duration'] + EPG_GAP) > now:
+                        plot += + get_plot_line(last_start, last_program['title'])
                         items_left -= 1
                         last_program = None
-                plot += get_plot_line(start,program['title'])
+                plot += get_plot_line(start, program['title'])
                 items_left -= 1
                 if items_left == 0:
                     break
             else:
                 last_program = program
         plot = plot[:-4]
-
-    info = {}
 
     info = utils.call(sl, lambda: sl.channel_info(lid))
 
@@ -84,12 +85,13 @@ def play(sl, lid, stationid, askpin):
         if is_helper.check_inputstream():
             playitem = xbmcgui.ListItem(path=info['path'])
             if plot:
-                playitem.setInfo('video', {'plot':plot})
+                playitem.setInfo('video', {'plot': plot})
             playitem.setProperty('inputstreamaddon', is_helper.inputstream_addon)
             playitem.setProperty('inputstream.adaptive.manifest_type', info['protocol'])
             playitem.setProperty('inputstream.adaptive.license_type', info['drm'])
             playitem.setProperty('inputstream.adaptive.license_key', info['key'])
             xbmcplugin.setResolvedUrl(_handle, True, playitem)
+
 
 def router(args, sl):
     if args:
